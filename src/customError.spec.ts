16688e31dfbd1cd1ddab3b953e7f4f049e9b3716
Reece Kidd <reecekidd95@gmail.com>
2020-09-14 10:19:20 +0100

    refactor: Updated getUserMiddlewares and customError unit tests

--- src/customError.spec.ts
+++ src/customError.spec.ts
@@ -2,6 +2,17 @@
 import { CustomError, ErrorType } from './customError';
 
 describe('customError', () => {
+    test(`creates correct error when type is set to GetUserNoUserFound`, () => {
+        expect.assertions(3);
+
+        const customError = new CustomError(ErrorType.GetUserNoUserFound);
+        const { code, message, httpStatusCode } = customError;
+
+        expect(code).toBe(`400-01`);
+        expect(message).toBe('User does not exist.');
+        expect(httpStatusCode).toBe(400);
+    });
+
     test(`creates correct error when type is set to InternalServerError`, () => {
         expect.assertions(3);
 
@@ -9,6 +20,50 @@ describe('customError', () => {
         const { code, message, httpStatusCode } = customError;
 
         expect(code).toBe(`500-01`);
+        expect(message).toBe('Internal Server Error.');
+        expect(httpStatusCode).toBe(500);
+    });
+
+    test(`creates correct error when type is set to SendUserMiddleware`, () => {
+        expect.assertions(3);
+
+        const customError = new CustomError(ErrorType.SendUserMiddleware);
+        const { code, message, httpStatusCode } = customError;
+
+        expect(code).toBe(`500-02`);
+        expect(message).toBe('Internal Server Error.');
+        expect(httpStatusCode).toBe(500);
+    });
+
+    test(`creates correct error when type is set to RetrieveUserMiddleware`, () => {
+        expect.assertions(3);
+
+        const customError = new CustomError(ErrorType.RetrieveUserMiddleware);
+        const { code, message, httpStatusCode } = customError;
+
+        expect(code).toBe(`500-03`);
+        expect(message).toBe('Internal Server Error.');
+        expect(httpStatusCode).toBe(500);
+    });
+
+    test(`creates correct error when type is set to CreateUserFromRequestMiddleware`, () => {
+        expect.assertions(3);
+
+        const customError = new CustomError(ErrorType.CreateUserFromRequestMiddleware);
+        const { code, message, httpStatusCode } = customError;
+
+        expect(code).toBe(`500-04`);
+        expect(message).toBe('Internal Server Error.');
+        expect(httpStatusCode).toBe(500);
+    });
+
+    test(`creates correct error when type is set to SendFormattedUserMiddleware`, () => {
+        expect.assertions(3);
+
+        const customError = new CustomError(ErrorType.SendFormattedUserMiddleware);
+        const { code, message, httpStatusCode } = customError;
+
+        expect(code).toBe(`500-05`);
         expect(message).toBe('Internal Server Error.');
         expect(httpStatusCode).toBe(500);
     });
--- src/customError.ts
+++ src/customError.ts
@@ -1,9 +1,13 @@
 /* eslint-disable */
 import { ResponseCodes } from './Server/responseCodes';
-import { getServiceConfig } from './getServiceConfig';
 
 export enum ErrorType {
     InternalServerError,
+    SendUserMiddleware,
+    RetrieveUserMiddleware,
+    GetUserNoUserFound,
+    CreateUserFromRequestMiddleware,
+    SendFormattedUserMiddleware,
 }
 
 const internalServerMessage = 'Internal Server Error.';
@@ -23,6 +27,41 @@ export class CustomError extends Error {
 
     private createCustomErrorData(type: ErrorType): { code: string; message: string; httpStatusCode: ResponseCodes } {
         switch (type) {
+            case ErrorType.GetUserNoUserFound:
+                return {
+                    code: `${ResponseCodes.badRequest}-01`,
+                    message: 'User does not exist.',
+                    httpStatusCode: ResponseCodes.badRequest,
+                };
+
+            case ErrorType.SendUserMiddleware:
+                return {
+                    code: `${ResponseCodes.warning}-02`,
+                    message: internalServerMessage,
+                    httpStatusCode: ResponseCodes.warning,
+                };
+
+            case ErrorType.RetrieveUserMiddleware:
+                return {
+                    code: `${ResponseCodes.warning}-03`,
+                    message: internalServerMessage,
+                    httpStatusCode: ResponseCodes.warning,
+                };
+
+            case ErrorType.CreateUserFromRequestMiddleware:
+                return {
+                    code: `${ResponseCodes.warning}-04`,
+                    message: internalServerMessage,
+                    httpStatusCode: ResponseCodes.warning,
+                };
+
+            case ErrorType.SendFormattedUserMiddleware:
+                return {
+                    code: `${ResponseCodes.warning}-05`,
+                    message: internalServerMessage,
+                    httpStatusCode: ResponseCodes.warning,
+                };
+
             default:
                 return {
                     code: `${ResponseCodes.warning}-01`,
